85104ea69a1853711c7ae1639ea1a9d8b457ab7f
suchait gaurav <suchait.gaurav.ctr@sabre.com>
2021-06-13 13:26:47 +0530

    Adding all changes.

--- src/main/java/com/infogain/gcp/poc/consumer/TmgSubscriberServiceApplication.java
+++ src/main/java/com/infogain/gcp/poc/consumer/TmgSubscriberServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsumerServiceApplication {
+public class TmgSubscriberServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsumerServiceApplication.class, args);
+		SpringApplication.run(TmgSubscriberServiceApplication.class, args);
 	}
 
 }
